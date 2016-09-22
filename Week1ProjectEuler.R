a612aa3da07eb61a935797f494d5ea7a72dd7a53
Eric Corwin <eric.corwin@gmail.com>
2016-09-22 16:14:09 -0700

    Function definitions for Week1 Project Euler assignment

---
+++ Week1ProjectEuler.R
@@ -0,0 +1,29 @@
+Euler1 = function(maxN = 1000) {
+  #Return the sum of all natural numbers less than maxN that are either multiples of 3 or 5
+  
+}
+
+Euler2 = function(maxVal = 4000000) {
+  #Return the sum of all even-valued fibonacci numbers whose values are less than maxVal
+  
+}
+
+Euler3 = function(number = 600851475143) {
+  #Return the largest prime factor of the number
+  
+}
+
+Euler4 = function(digits = 3) {
+  #Return the largest palindrome made from the product of two "digits"-digit numbers
+  
+}
+
+Euler5 = function(maxN = 20) {
+  #Return the smallest positive number that is evenly divisible by all of the numbers from 1 to maxN
+  
+}
+
+Euler10 = function(maxVal = 2000000) {
+  #Return the sum of all primes less than maxVal
+  
+}
